da70b5ef3e259b242b4ef6b758a2519e7d89029a
Vortique <wayw04321@gmail.com>
2026-01-12 21:23:09 +0300

    Add memcpy_func.c.

---
+++ memcpy_func.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void *my_memcpy(void *dest, void *src, int byte_count);
+
+int main(void) {
+    int a[5] = { 1, 2, 3, 4, 5 };
+    int b[5];
+
+    my_memcpy(b, a, sizeof b);
+
+    int *p = &b[0];
+    int *end = p + 5;
+
+    for (; p < end; p++) {
+        printf("%d\n", *p);
+    }
+
+    return 0;
+}
+
+void *my_memcpy(void *dest, void *src, int byte_count) {
+    char *s = src, *d = dest;
+
+    while (byte_count--) {
+        *d++ = *s++;
+    }
+
+    return dest;
+}
